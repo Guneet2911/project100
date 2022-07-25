7faa56133f5321e1abbc9bf4d7971460950950be
Guneet2911 <98748570+Guneet2911@users.noreply.github.com>
2022-07-25 17:50:56 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDk_-KzO6vbfhXBBgUrea0CYDlD3RcXqHU",
+    authDomain: "lets-chat-15de2.firebaseapp.com",
+    databaseURL: "https://lets-chat-15de2-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-15de2",
+    storageBucket: "lets-chat-15de2.appspot.com",
+    messagingSenderId: "669950423814",
+    appId: "1:669950423814:web:6d8116ef437432f651a487",
+    measurementId: "G-D3F1521V2W"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
